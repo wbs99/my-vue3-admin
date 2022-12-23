f0f802f01dd386d57c0784cd1c78445389557f1e
dingtalk_xsgrpl <1134954328@qq.com>
2022-12-23 10:58:27 +0800

    finish

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,13 @@
+/// <reference types="vite/client" />
+
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+}
 
-
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
